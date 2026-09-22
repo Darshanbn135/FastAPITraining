9aa2bbe6f442e22a6dae815a738adfe2ed5baabd
Darshanbn135 <darshanbndarshan11@gmail.com>
2026-09-22 13:30:31 +0530

    Path parameters with type hint

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -13,3 +13,13 @@ def health():
 @app.post("/create")
 def create_something():
     return {"message": "Data created "}
+
+#path parameter
+@app.get("/Student/{usn}")
+def get_student(usn):
+    return {"Result": "Distinction", "USN": usn}
+
+#path parameter with type Hint
+@app.get("/Candidate/{rollno}")
+def get_candidate(rollno: int):
+    return {"Result": "Distinction", "roll No": rollno, "type": str(rollno)}
