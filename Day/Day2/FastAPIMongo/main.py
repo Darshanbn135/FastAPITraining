e0463b03489251af53b4dab4e77f739f9f6df41d
Darshanbn135 <darshanbndarshan11@gmail.com>
2026-09-22 16:02:37 +0530

    pymango

---
+++ Day/Day2/FastAPIMongo/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pymongo import AsyncIOMotorClient
+app = FastAPI()
+client = AsyncIOMotorClient("mongodb://localhost:27017/")
+db = client["College"]
+
+# select collection
+students_collection = db["Students"]
+
+@app.get("/")
+async def home():
+    return {
+        "message": "FastAPI with MongoDB is running",
+    }
+@app.get("/health")
+async def health():
+    result = await db.command("ping")
+    return {"mongodb":"connected", "ping": result}
