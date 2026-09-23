2d69b0d3208903c11b273e366b38e76d4d94ca14
Darshanbn135 <darshanbndarshan11@gmail.com>
2026-09-23 13:01:48 +0530

    cp:1

--- Day/Day3/Capstone Project/app/main.py
+++ Day/Day3/Capstone Project/app/main.py
@@ -1,4 +1,4 @@
-# This file is the "entry point" of the application
+# This file is the "entry point" of the application.
 from fastapi import FastAPI
 
 from app.config import settings
@@ -7,15 +7,15 @@ from app.database import ping_database
 # Creating FastAPI app instance
 app = FastAPI(title=settings.APP_NAME)
 
-# This function runs once when the server starts. It checks the DB connection.
+# This connection runs once when the server starts. It checks if the MongoDB connection is alive and raises an error if it is not.
 @app.on_event("startup")
 def on_startup() -> None:
     if not ping_database():
-        raise RuntimeError("Could not connect to MongoDB")
-    print(f"[startup]Connected to MongoDB. App:{settings.APP_NAME}")
+        raise RuntimeError("Could not connect to the MongoDB.")
+    print(f"[startup] Connected to MongoDB: {settings.APP_NAME}")
 
-# Checks basic health-check API endpoint & confirms 
-# GET / is running & reachable. (/ is considered as 'root')
-@app.get("/",tags=["Health"])
+# Checks basic health-chek API endpoins & confirms 
+# Get / is running & readble. (/ is considered as 'root)
+@app.get("/", tags=["Health"])
 def health_check():
-    return {"status":"ok","app":settings.APP_NAME}
+    return {"status": "ok", "message": f"{settings.APP_NAME} is running."}
